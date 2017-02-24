c37db1d9c17b2f066045174d8b52f4c3ad9ac90b
unknown <lobo__911@hotmail.comclear>
2017-02-24 14:55:43 -0200

    Fix installation controller

--- config/larabuild.php
+++ config/larabuild.php
@@ -36,20 +36,6 @@ return [
 		],
 	],
 
-	/**
-	 * Flysystem's permissions config
-	 */
-	'file_permissions' => [
-	    'file' => [
-	        'public' => 0644,
-	        'private' => 0644,
-	    ],
-	    'dir' => [
-	        'public' => 0755,
-	        'private' => 0755,
-	    ],
-    ],
-
     /**
      * NOT IMPLEMENTED
      * Parameters passed to each hook: BuildCommand $command, MountManager $manager, array $config
